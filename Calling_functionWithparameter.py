05176d21a7368bd031a5f23b6b153472603191f3
MasumTech <masumrezadiu@gmail.com>
2020-01-21 15:13:20 +0600

    Calling function with parameter in Python

---
+++ Calling_functionWithparameter.py
@@ -0,0 +1,20 @@
+from variableLengthParameterExample import sumValues
+
+
+a = sumValues(10,20)
+
+print("sumValues(10,20) = ", a);
+
+a = sumValues(10,20,1);
+
+print("sumValues(10,20,1) = ",a);
+
+
+a = sumValues(10,20,1,2);
+
+print("sumValues(10,20,1,2) = ",a);
+
+a = sumValues(10,20,1,2,3,4,5);
+
+
+print("sumValues(10,20,1,2,3,4,5) = ", a);
