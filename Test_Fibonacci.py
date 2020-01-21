35f8a07104248a9ca984e5970ee99fa00ba6e441
MasumTech <masumrezadiu@gmail.com>
2020-01-21 15:20:07 +0600

    Test Fibonacci in Python

---
+++ Test_Fibonacci.py
@@ -0,0 +1,18 @@
+import fibonacci
+print("Hello, I am inside program.py!")
+print(fibonacci.__name__)
+
+ 
+
+
+
+
+'''
+import fibonacci
+print('Hello, I am inside fibbonacci.py!')
+
+n = fibonacci.find_fib(15)
+
+print('15th fibonacci number is,', n)
+ 
+'''
