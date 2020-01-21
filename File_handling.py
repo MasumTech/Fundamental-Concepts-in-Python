ab6cbecd9e4c6ae1b9b583bf9c703d1f8a7f3b96
MasumTech <masumrezadiu@gmail.com>
2020-01-21 15:16:06 +0600

    File Handling in Python

---
+++ File_handling.py
@@ -0,0 +1,32 @@
+with open("file.txt", "r") as test:
+  print(test.read())
+
+'''
+lines = ["This is first line.", "This is second line.", "This is third line."]
+ 
+with open("file.txt", "w") as fp:
+    for line in lines:
+        fp.write(line+"\n")
+
+
+with open("file.txt", "r") as fp:
+     content = fp.read()
+     print(content)
+        
+
+with open("file.txt", "r") as fp:
+    lines = fp.readlines()
+    print(lines)
+    for line in lines:
+        print(line)
+
+
+with open("file.txt", 'r') as fb:
+  for line in  fb:
+    print(line)
+
+'''
+        
+
+ 
+
